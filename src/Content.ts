638dd62ae4e22d282f27ea4e27f7213b2dd35d1e
gergely19 <mgergo0811@gmail.com>
2020-03-31 10:58:57 +0200

    up

--- src/Content.ts
+++ src/Content.ts
@@ -24,10 +24,46 @@ export default class Content {
         const params = url.parse(req.url as string, true).query;
 
         // Kezd a kódolást innen -->
-        res.write("Hello Jedlik, hello 9.A");
-        res.write("<h2 style='color:red;'>TipeScript</h2>");
-        res.write("próba\n");
-        res.write("alma\n");
+
+        // res.write("Hello Jedlik, hello 9.A");
+        // res.write("<h2 style='color:red;'>TipeScript</h2>");
+        // res.write("próba\n");
+        // res.write("alma\n");
+
+        // let x = 12; //változó definíció
+        // x = 20;
+        // res.write(`${x}\n`);
+        // res.write(x.toString() + "\n");
+        // const szöveg = "alma";
+        // res.write(szöveg + "\n");
+        // let esik: boolean;
+        // esik = true;
+        // esik = false;
+        // res.write(esik.toString() + "\n");
+        // res.write(`${esik}\n`);
+
+        res.write("Téglalap területe és kerülete:\n");
+        res.write("a= ");
+        let oldalA: number = parseInt(params.inputa as string);
+        if (isNaN(oldalA)) {
+            oldalA = 20;
+        }
+        res.write(`<input type='text' name='inputa' value=${oldalA} style='width:5em;' onChange='this.form.submit();'>\n`);
+
+        res.write("Téglalap területe és kerülete:\n");
+        res.write("b= ");
+        let oldalB: number = parseInt(params.inputa as string);
+        if (isNaN(oldalB)) {
+            oldalB = 30;
+        }
+        res.write(`<input type='text' name='inputb' value=${oldalB} style='width:5em;' onChange='this.form.submit();'>\n`);
+
+        const terület = oldalA * oldalB;
+        const kerület = 2 * (oldalA * oldalB);
+        res.write(`terület = ${terület}\n`);
+        res.write(`kerület = ${kerület}\n`);
+
+        //Az algoritmusokban a változók a megadott típusú értékek (adatok) tárolására használt memóriatartományok elnevezései.
         // <---- Fejezd be a kódolást
 
         res.write("</pre></form></body></html>");
