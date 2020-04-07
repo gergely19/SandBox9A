72b41a3ed3530635ab0e6d3bf073fbfd01d7b76f
gergely19 <mgergo0811@gmail.com>
2020-04-07 10:59:46 +0200

    up

--- src/Content.ts
+++ src/Content.ts
@@ -107,6 +107,39 @@ export default class Content {
                 res.write("Ez nem osztályzat\n");
                 break;
         }
+        res.write("Másodfokú egyenlet gyökei");
+        res.write("Kérem az együtthatókat !");
+        res.write("a = ");
+        const a: number = double.Parse(Console.ReadLine());
+        res.write("b = ");
+        const b: number = double.Parse(Console.ReadLine());
+        res.write("c = ");
+        const c: number = double.Parse(Console.ReadLine());
+        if (a != 0) {
+            if (Math.pow(b, 2) >= 4 * a * c) {
+                if (Math.pow(b, 2) > 4 * a * c) {
+                    res.write("Két gyök!");
+                    const x1: number = -b + Math.sqrt(Math.pow(b, 2) - 4 * a * c) / (2 * a);
+                    const x2: number = -b - Math.sqrt(Math.pow(b, 2) - 4 * a * c) / (2 * a);
+                    res.write(`x1 = ${x1}`);
+                    res.write(`x2 = ${x2}`);
+                } else {
+                    res.write("Egy gyök!");
+                    const x: number = -b / (2 * a);
+                    res.write("x = {0}", x);
+                }
+            } else res.write("Nincs valós gyök!");
+        } else {
+            res.write("Nem másodfokú!");
+            if (b != 0) {
+                const x: number = -c / b;
+                res.write("x = {0}", x);
+            } else {
+                if (c != 0) res.write("Ellentmondás !");
+                else res.write("Azonosság !");
+            }
+        }
+        res.ReadLine();
         //Az algoritmusokban a változók a megadott típusú értékek (adatok) tárolására használt memóriatartományok elnevezései.
         // <---- Fejezd be a kódolást
 
